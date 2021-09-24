f652fba44a5a01113b094e9fdc4099b04b8176d0
Noé Pécault <noepec1@gmail.com>
2021-09-24 19:51:17 +0200

    chore: Created AntEngine project

---
+++ AntEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AntEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
