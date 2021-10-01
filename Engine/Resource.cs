8f3d9349057d68b0369ad90bfc95cd4d19a4726d
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-10-01 21:08:13 +0200

    feat(Resource): Added Resource.cs

---
+++ Engine/Resource.cs
@@ -0,0 +1,18 @@
+﻿namespace AntEngine
+{
+    public class Resource
+    {
+        private int _amount;
+
+        public Resource(int a)
+        {
+            _amount = a;
+        }
+
+        public int Amount
+        {
+            get => _amount;
+            set => _amount = value;
+        }
+    }
+}
