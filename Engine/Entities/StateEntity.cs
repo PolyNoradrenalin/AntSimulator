fe077c311eccbe95f46ee79c03ed1a8db578f835
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-01 22:37:50 +0200

    feat(state): Added StateEntity class

---
+++ Engine/Entities/StateEntity.cs
@@ -0,0 +1,10 @@
+namespace AntEngine.Entities
+{
+    /// <summary>
+    /// An entity that can be a finite set of states and change state with specific transitions.
+    /// </summary>
+    public abstract class StateEntity
+    {
+        
+    }
+}
