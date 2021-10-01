36977e822fd34c42397f57e0b454c1eb0d8441df
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-10-01 20:58:42 +0200

    Refactor(Colliders): Added TODOs to Collider children

--- Engine/Colliders/CircleCollider.cs
+++ Engine/Colliders/CircleCollider.cs
@@ -4,16 +4,19 @@
     {
         protected override bool checkCollision(CircleCollider circleCollider)
         {
+            //TODO : Implement
             throw new System.NotImplementedException();
         }
 
         protected override bool checkCollision(RectangleCollider rectCollider)
         {
+            //TODO : Implement
             throw new System.NotImplementedException();
         }
 
         protected override bool checkCollision(WorldCollider worldCollider)
         {
+            //TODO : Implement
             throw new System.NotImplementedException();
         }
     }
