c289dd3ce784a1e39cae82a7370255c0066c40e7
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-15 19:04:58 +0200

    feat(colony): Spawning of LivingEntity

--- Engine/Entities/Colonies/Colony.cs
+++ Engine/Entities/Colonies/Colony.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 using AntEngine.Entities.States;
 using AntEngine.Entities.States.Living;
 using AntEngine.Maths;
@@ -11,10 +14,10 @@ namespace AntEngine.Entities.Colonies
     /// Entity that can spawn other entities with resources.
     /// </summary>
     /// <typeparam name="E">The type of the entity to spawn.</typeparam>
-    public class Colony<E> : LivingEntity where E : LivingEntity, new()
+    public class Colony<E> : LivingEntity where E : LivingEntity
     {
         private const string ColonyDefaultName = "Colony";
-        
+
         private List<E> _population;
         private Dictionary<Resource, int> _stockpile;
         private Dictionary<Resource, int> _spawnCost;
@@ -32,6 +35,12 @@ namespace AntEngine.Entities.Colonies
             _population = new List<E>();
             _stockpile = new Dictionary<Resource, int>();
             _spawnCost = new Dictionary<Resource, int>();
+
+            SpawnMethod = (s, t, w, c) =>
+            {
+                Console.WriteLine("No spawn function defined, set the SpawnMethod.");
+                return null;
+            };
         }
 
         /// <summary>
@@ -39,14 +48,91 @@ namespace AntEngine.Entities.Colonies
         /// This is basically the entities that have been spawned by the colony.
         /// </summary>
         public IList<E> Population => _population.ToImmutableList();
+
         /// <summary>
         /// Current stockpile of the colony.
         /// The resources that can be used to spawn more entities.
         /// </summary>
         public IDictionary<Resource, int> Stockpile => _stockpile.ToImmutableDictionary();
+
         /// <summary>
         /// The cost in resources of spawning one entity.
         /// </summary>
         public IDictionary<Resource, int> SpawnCost => _spawnCost.ToImmutableDictionary();
+
+        /// <summary>
+        /// Distance of spawning from the colony center.
+        /// </summary>
+        private float SpawnRadius { get; set; } = 1F;
+
+        /// <summary>
+        /// Where an entity should spawn.
+        /// </summary>
+        protected virtual Vector2 SpawnPosition
+        {
+            get
+            {
+                float angle = (float)new Random().NextDouble();
+                float x = MathF.Cos(angle);
+                float y = MathF.Sin(angle);
+
+                return Transform.Position + new Vector2(x, y) * SpawnRadius;
+            }
+        }
+
+        /// <summary>
+        /// Instructions to spawn an entity of the colony.
+        /// </summary>
+        public Func<string, Transform, World, Colony<E>, E> SpawnMethod { get; set; }
+
+        /// <summary>
+        /// Spawn entities stopping when at count or when stockpile has not enough resources.
+        /// </summary>
+        /// <param name="count">Number of entities to spawn</param>
+        public virtual void Spawn(int count = 1)
+        {
+            for (int i = 0; i < count && HasEnoughResources(); i++)
+            {
+                ConsumeResources();
+
+                E pop = SpawnMethod("", new Transform(SpawnPosition, 0, Vector2.One), World, this);
+                if (pop != null) _population.Add(pop);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the colony has enough resources in its stockpile to spawn an entity.
+        /// </summary>
+        /// <returns>True if it can, false otherwise</returns>
+        protected bool HasEnoughResources()
+        {
+            foreach ((Resource resource, int cost) in _spawnCost)
+            {
+                if (_stockpile.ContainsKey(resource))
+                {
+                    if (_stockpile[resource] < cost) return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Removes from the stockpile _spawnCost resources. 
+        /// </summary>
+        /// <returns>True if success, false otherwise</returns>
+        protected void ConsumeResources()
+        {
+            if (!HasEnoughResources()) return;
+            
+            foreach ((Resource resource, int cost) in _spawnCost)
+            {
+                _stockpile[resource] -= cost;
+            }
+        }
     }
 }
