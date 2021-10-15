55fc937be795a5115df09f8b770b0c9c44fac0be
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-15 17:53:48 +0200

    feat(colony): Added Colony properties

--- Engine/Entities/Colonies/Colony.cs
+++ Engine/Entities/Colonies/Colony.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using AntEngine.Entities.States;
+using AntEngine.Entities.States.Living;
 using AntEngine.Maths;
+using AntEngine.Resources;
 
 namespace AntEngine.Entities.Colonies
 {
@@ -9,16 +13,40 @@ namespace AntEngine.Entities.Colonies
     /// <typeparam name="E">The type of the entity to spawn.</typeparam>
     public class Colony<E> : LivingEntity where E : LivingEntity, new()
     {
-        public Colony(World world) : base(world)
+        private const string ColonyDefaultName = "Colony";
+        
+        private List<E> _population;
+        private Dictionary<Resource, int> _stockpile;
+        private Dictionary<Resource, int> _spawnCost;
+
+        public Colony(World world) : this(ColonyDefaultName, new Transform(), world)
         {
         }
 
-        public Colony(string name, Transform transform, World world) : base(name, transform, world)
+        public Colony(string name, Transform transform, World world) : this(name, transform, world, new IdleState())
         {
         }
 
         public Colony(string name, Transform transform, World world, IState initialState) : base(name, transform, world, initialState)
         {
+            _population = new List<E>();
+            _stockpile = new Dictionary<Resource, int>();
+            _spawnCost = new Dictionary<Resource, int>();
         }
+
+        /// <summary>
+        /// Current population of the colony.
+        /// This is basically the entities that have been spawned by the colony.
+        /// </summary>
+        public IList<E> Population => _population.ToImmutableList();
+        /// <summary>
+        /// Current stockpile of the colony.
+        /// The resources that can be used to spawn more entities.
+        /// </summary>
+        public IDictionary<Resource, int> Stockpile => _stockpile.ToImmutableDictionary();
+        /// <summary>
+        /// The cost in resources of spawning one entity.
+        /// </summary>
+        public IDictionary<Resource, int> SpawnCost => _spawnCost.ToImmutableDictionary();
     }
 }
