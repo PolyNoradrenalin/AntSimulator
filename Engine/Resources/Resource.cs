33f9f4f7fe4af03d89d0f32a49a7cd9b14bd6b6a
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-19 01:05:05 +0200

    feat(resource): Added Name property
    Also added documentation

--- Engine/Resources/Resource.cs
+++ Engine/Resources/Resource.cs
@@ -7,12 +7,20 @@ namespace AntEngine.Resources
     /// </summary>
     public class Resource
     {
-        public Resource(String type)
+        public Resource(string type, string name)
         {
             Type = type;
+            Name = name;
         }
-        
-        public virtual String Name { get; set; }
-        public String Type { get; private set; }
+
+        /// <summary>
+        /// Unique identifier of the resource. <br />
+        /// Resources can have the same name but still be different.
+        /// </summary>
+        public string Type { get; private set; }
+        /// <summary>
+        /// Display name of the resource.
+        /// </summary>
+        public string Name { get; private set; }
     }
 }
