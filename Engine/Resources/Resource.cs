6e8c715b76e078319edd1eb9b144092936ec751f
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-19 01:06:45 +0200

    feat(resource): Added Resource Equals override

--- Engine/Resources/Resource.cs
+++ Engine/Resources/Resource.cs
@@ -22,5 +22,23 @@ namespace AntEngine.Resources
         /// Display name of the resource.
         /// </summary>
         public string Name { get; private set; }
+
+        protected bool Equals(Resource other)
+        {
+            return Type == other.Type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Resource)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Type != null ? Type.GetHashCode() : 0);
+        }
     }
 }
