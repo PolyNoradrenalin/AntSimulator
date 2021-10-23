a8bc2f20a9175ad73bbd7b8404e9f87ded530df2
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-23 22:57:32 +0200

    test(rectangle-collider): Added tests for RectangleCollider

--- Tests/Engine/RectangleColliderTests.cs
+++ Tests/Engine/RectangleColliderTests.cs
@@ -23,15 +23,75 @@ namespace Tests.Engine
         }
         
         [Fact]
+        public void CheckCollision_ParentRotated_ShouldNotCollide()
+        {
+            RectangleCollider col1 = new(new Transform(), new Transform());
+            RectangleCollider col2 = new(new Transform(), new Transform());
+
+            col1.ParentTransform.Position = new Vector2(-100, 0);
+            col1.ParentTransform.Rotation = MathF.PI * 0.5f;
+            
+            col2.ParentTransform.Position = new Vector2(100, 0);
+
+            Assert.False(col1.checkCollision(col2));
+        }       
+        
+        [Fact]
+        public void CheckCollision_ChildRotated_ShouldNotCollide()
+        {
+            RectangleCollider col1 = new(new Transform(), new Transform());
+            RectangleCollider col2 = new(new Transform(), new Transform());
+
+            col1.ParentTransform.Position = new Vector2(-100, 0);
+            col1.ColliderTransform.Rotation = MathF.PI * 0.5f;
+            
+            col2.ParentTransform.Position = new Vector2(100, 0);
+
+            Assert.False(col1.checkCollision(col2));
+        }
+        
+        [Fact]
         public void CheckCollision_WithoutRotation_ShouldCollide()
         {
             RectangleCollider col1 = new(new Transform(), new Transform());
             RectangleCollider col2 = new(new Transform(), new Transform());
 
             col1.ParentTransform.Position = new Vector2(-1, 0);
+            col1.ParentTransform.Scale = new Vector2(10, 1);
+            
+            col2.ParentTransform.Position = new Vector2(1, 0);
+            
+            Assert.True(col1.checkCollision(col2));
+        }
+        
+        [Fact]
+        public void CheckCollision_ParentRotated_ShouldCollide()
+        {
+            RectangleCollider col1 = new(new Transform(), new Transform());
+            RectangleCollider col2 = new(new Transform(), new Transform());
+
+            col1.ParentTransform.Position = new Vector2(-1, 0);
+            col1.ParentTransform.Rotation = MathF.PI * 0.5f;
+            col1.ParentTransform.Scale = new Vector2(10, 1);
+
             col2.ParentTransform.Position = new Vector2(1, 0);
 
+
+            Assert.True(col1.checkCollision(col2));
+        }
+        
+        [Fact]
+        public void CheckCollision_ChildRotated_ShouldCollide()
+        {
+            RectangleCollider col1 = new(new Transform(), new Transform());
+            RectangleCollider col2 = new(new Transform(), new Transform());
+
+            col1.ParentTransform.Position = new Vector2(-1, 0);
+            col1.ColliderTransform.Rotation = MathF.PI * 0.5f;
             col1.ParentTransform.Scale = new Vector2(10, 1);
+
+            col2.ParentTransform.Position = new Vector2(1, 0);
+
 
             Assert.True(col1.checkCollision(col2));
         }
