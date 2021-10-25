5a9310b125a04235e2d0eb861bdb0aeb18303cae
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-26 01:16:19 +0200

    feat(window): Added placeholders components

--- App/ViewModels/MainWindowViewModel.cs
+++ App/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using System.Windows.Input;
+using Avalonia.Diagnostics;
+using Avalonia.Logging;
+using ReactiveUI;
 
 namespace AntUI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private string testButtonText;
+
+        public string TestButtonText
+        {
+            get => testButtonText;
+            set => this.RaiseAndSetIfChanged(ref testButtonText, value);
+        }
+        
+        public MainWindowViewModel()
+        {
+            testButtonText = "Test Button";
+        }
+
+        public void OnTestClick(string parameter)
+        {
+            TestButtonText = "HAHAHA I'M A TEST BUTTON :D";
+        }
     }
 }
--- App/Views/MainWindow.axaml.cs
+++ App/Views/MainWindow.axaml.cs
@@ -1,11 +1,16 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace AntUI.Views
 {
     public partial class MainWindow : Window
     {
+        private Button _testButton;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +22,7 @@ namespace AntUI.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            _testButton = this.FindControl<Button>("testButton");
         }
     }
 }
