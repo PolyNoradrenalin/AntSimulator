18f6c095b564b857377d98ed42ea149704c28128
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-10-26 15:57:30 +0200

    feat(Pheromones): FoodPheromones and HomePheromones added

---
+++ Engine/Entities/Pheromones/HomePheromone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AntEngine.Maths;
+
+namespace AntEngine.Entities.Pheromones
+{
+    /// <summary>
+    /// Pheromone released while an Ant is searching for food.
+    /// </summary>
+    public class HomePheromone : Pheromone
+    {
+        public HomePheromone(World world) : this(DefaultPheromoneName, new Transform(), world, new TimeSpan())
+        {
+        }
+        
+        public HomePheromone(World world, TimeSpan maxTimeSpan) : this(DefaultPheromoneName, new Transform(), world, maxTimeSpan)
+        {
+        }
+
+        public HomePheromone(string name, Transform transform, World world, TimeSpan maxTimeSpan) : base(name, transform, world, maxTimeSpan)
+        {
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
