3e029c639e8ef1ec6932504c58a354b7a3967a0a
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-26 16:53:58 +0200

    feat(render): Added SimulationCanvas control

---
+++ App/Views/SimlationCanvas.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace AntUI.Views
+{
+    public class SimulationCanvas : UserControl
+    {
+        public SimulationCanvas()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+        }
+    }
+}
