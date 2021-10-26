c1bb242e0899fb8ab18493ed4899f99014398b3b
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-10-26 17:12:06 +0200

    feat(render): Added IRenderer interface

--- App/Views/SimlationCanvas.axaml.cs
+++ App/Views/SimlationCanvas.axaml.cs
@@ -1,15 +1,19 @@
-﻿using Avalonia;
+﻿using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using IRenderer = AntUI.Renderers.IRenderer;
 
 namespace AntUI.Views
 {
     public class SimulationCanvas : UserControl
     {
+        private ISet<IRenderer> _renderers;
+        
         public SimulationCanvas()
         {
             InitializeComponent();
+            _renderers = new HashSet<IRenderer>();
         }
 
         private void InitializeComponent()
@@ -20,6 +24,11 @@ namespace AntUI.Views
         public override void Render(DrawingContext context)
         {
             base.Render(context);
+
+            foreach (IRenderer renderer in _renderers)
+            {
+                renderer.Draw(context);
+            }
         }
     }
 }
