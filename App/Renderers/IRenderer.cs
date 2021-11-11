1906718e86c853706c0869070af116d54cc569d7
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-11 14:28:06 +0100

    feat: added renderer interface

---
+++ App/Renderers/IRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.Renderers
+{
+    /// <summary>
+    /// Interface for a renderer
+    /// </summary>
+    public interface IRenderer
+    {
+        void Render(SpriteBatch spriteBatch, GraphicsDeviceManager gdm);
+    }
+}
