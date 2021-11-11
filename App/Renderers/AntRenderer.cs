52cabc73082cfa861dcb7140cfcb743f87a3cce3
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-11 14:29:02 +0100

    feat: added ant renderer

---
+++ App/Renderers/AntRenderer.cs
@@ -0,0 +1,22 @@
+﻿using AntEngine.Entities;
+using AntEngine.Entities.Ants;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.Renderers
+{
+    /// <summary>
+    /// Renderer for an Ant Entity.
+    /// </summary>
+    public class AntRenderer : EntityRenderer
+    {
+        public AntRenderer(Ant e) : base(e)
+        {
+        }
+
+        public override void Render(SpriteBatch spriteBatch, GraphicsDeviceManager gdm)
+        {
+            // TODO: Implement
+        }
+    }
+}
