4dea8b9709b96a5be7a043f365d95f9df07b4af4
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-11 14:31:59 +0100

    feat: added simframe class

---
+++ App/SimFrame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.Renderers
+{
+    public class SimFrame : IRenderer
+    {
+        private List<IRenderer> renderers;
+
+        public SimFrame()
+        {
+            renderers = new List<IRenderer>();
+        }
+        
+        public void Render(SpriteBatch spriteBatch, GraphicsDeviceManager gdm)
+        {
+            foreach (IRenderer r in renderers)
+            {
+                r.Render(spriteBatch, gdm);
+            }
+        }
+    }
+}
