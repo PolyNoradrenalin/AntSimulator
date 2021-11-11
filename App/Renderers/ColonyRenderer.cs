29706c504f20d367323d7a09bdc0fe5d5f6d6fe5
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-11 14:28:53 +0100

    feat: added colony renderer

---
+++ App/Renderers/ColonyRenderer.cs
@@ -0,0 +1,22 @@
+﻿using AntEngine.Entities;
+using AntEngine.Entities.Colonies;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.Renderers
+{
+    /// <summary>
+    /// Renderer for a Colony Entity.
+    /// </summary>
+    public class ColonyRenderer : EntityRenderer
+    {
+        public ColonyRenderer(Colony e) : base(e)
+        {
+        }
+
+        public override void Render(SpriteBatch spriteBatch, GraphicsDeviceManager gdm)
+        {
+            // TODO: Implement
+        }
+    }
+}
