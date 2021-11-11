701b8634daff41b988c9b9e2fc250f496449d41d
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-11 14:28:21 +0100

    feat: added default entity renderer

---
+++ App/Renderers/EntityRenderer.cs
@@ -0,0 +1,23 @@
+﻿using AntEngine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace App.Renderers
+{
+    /// <summary>
+    /// Represents the renderer for an abstract Entity class.
+    /// </summary>
+    public class EntityRenderer : IRenderer
+    {
+        protected Entity entity;
+        
+        public EntityRenderer(Entity e)
+        {
+            entity = e;
+        }
+        public virtual void Render(SpriteBatch spriteBatch, GraphicsDeviceManager gdm)
+        {
+            // TODO: Implement
+        }
+    }
+}
