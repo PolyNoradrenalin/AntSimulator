54007942c0b80d5fcca1080e84ace7e19b1b33c5
NarvinChana <narvin.chana@etu.univ-tours.fr>
2021-11-13 21:38:30 +0100

    feat: created skeleton CarryState to be implemented in a later MR

---
+++ Engine/Entities/States/Living/CarryState.cs
@@ -0,0 +1,36 @@
+﻿namespace AntEngine.Entities.States.Living
+{
+    public class CarryState : IState
+    {
+        private static CarryState _instance;
+
+        public static CarryState Instance
+        {
+            get
+            {
+                _instance ??= new CarryState();
+                return _instance;
+            }
+        }
+        
+        public void OnStateStart(StateEntity stateEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateUpdate(StateEntity stateEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateEnd(StateEntity stateEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IState Next(StateEntity stateEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
