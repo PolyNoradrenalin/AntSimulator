30695b17229cc8bb1cacc55117155204ec7f8c9e
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2021-11-21 20:20:10 +0100

    feat(search-state): Search state now avoid colliders not Circle

--- Engine/Entities/States/Living/SearchState.cs
+++ Engine/Entities/States/Living/SearchState.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using AntEngine.Colliders;
 using AntEngine.Entities.Ants;
 using AntEngine.Entities.Pheromones;
 using AntEngine.Entities.Strategies.Movement;
@@ -13,6 +15,9 @@ namespace AntEngine.Entities.States.Living
     /// </summary>
     public class SearchState : LivingState
     {
+        private const float ObstacleDetectRadius = 10F;
+        private const int ObstacleIndexDivisor = 5;
+        
         private static SearchState _instance;
 
         public new static SearchState Instance
@@ -32,6 +37,31 @@ namespace AntEngine.Entities.States.Living
             
             Ant ant = (Ant) stateEntity;
             PerceptionMap perceptionMap = ant.GetPerceptionMap<FoodPheromone>();
+            int maxDirIndex = ant.PerceptionMapPrecision;
+
+            List<Vector2> dirs = new();
+            dirs.Add(perceptionMap.Weights.Keys.ElementAt(0));
+            dirs.Add(perceptionMap.Weights.Keys.ElementAt(maxDirIndex / ObstacleIndexDivisor - 1));
+            dirs.Add(perceptionMap.Weights.Keys.ElementAt(maxDirIndex - maxDirIndex / ObstacleIndexDivisor - 1));
+
+            foreach (Vector2 dir in dirs)
+            {
+                Vector2 globalDir = new Vector2(
+                    MathF.Cos(ant.Transform.Rotation) * dir.X - MathF.Sin(ant.Transform.Rotation) * dir.Y,
+                    MathF.Sin(ant.Transform.Rotation) * dir.X + MathF.Cos(ant.Transform.Rotation) * dir.Y
+                );
+                
+                IList<Collider> collisions = new List<Collider>(
+                    stateEntity.World.CircleCast(
+                    stateEntity.Transform.Position + globalDir * ObstacleDetectRadius,
+                    ObstacleDetectRadius));
+                
+                collisions = collisions.Where(collider => collider is not CircleCollider).ToList();
+                if (collisions.Count > 0)
+                {
+                    perceptionMap.Weights[dir] -= 1F;
+                }
+            }
 
             ant.Move(ant.MovementStrategy.Move(perceptionMap));
 
