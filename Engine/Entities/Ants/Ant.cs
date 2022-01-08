d8adbdbb689ad1533d8e506bba4f645de07adde2
Noé Pécault <noe.pecault@etu.univ-tours.fr>
2022-01-08 20:12:45 +0100

    refactor: Created extension method Vector2.Angle

--- Engine/Entities/Ants/Ant.cs
+++ Engine/Entities/Ants/Ant.cs
@@ -102,11 +102,10 @@ namespace AntEngine.Entities.Ants
                 Vector2 antDir = Transform.GetDirectorVector();
                 Vector2 pheromoneDirection = e.Transform.Position - Transform.Position;
 
-                float angle = MathF.Atan2(pheromoneDirection.Y, Vector2.Dot(pheromoneDirection, Vector2.UnitX));
+                float angle = antDir.Angle(Vector2.UnitX);
                 angle = angle < 0F ? angle + 2 * MathF.PI : angle;
 
-                float angleDiff = MathF.Atan2(antDir.Y * pheromoneDirection.X - antDir.X * pheromoneDirection.X,
-                    antDir.X * pheromoneDirection.X + antDir.Y * pheromoneDirection.Y);
+                float angleDiff = antDir.Angle(pheromoneDirection);
 
                 int weightListIndex = (int) Math.Min(PerceptionDistance-1, (int) MathF.Floor(angle / (2 * MathF.PI / PerceptionMapPrecision)));
 
